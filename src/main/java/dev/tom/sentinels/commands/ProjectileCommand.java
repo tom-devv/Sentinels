c094aaae74620de70312e61204a192d301110fac
tom-devv <73348376+tom-devv@users.noreply.github.com>
2025-06-11 20:53:25 +0100

    flare item command

--- src/main/java/dev/tom/sentinels/commands/ProjectileCommand.java
+++ src/main/java/dev/tom/sentinels/commands/ProjectileCommand.java
@@ -1,16 +1,21 @@
 package dev.tom.sentinels.commands;
 
+
+
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import dev.tom.sentinels.data.DamagingAttributes;
-import dev.tom.sentinels.data.SentinelDataWrapper;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import dev.tom.sentinels.data.DamagingAttributes;
+import dev.tom.sentinels.data.FlareAttributes;
+import dev.tom.sentinels.data.SentinelDataWrapper;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class ProjectileCommand {
 
@@ -20,19 +25,41 @@ public class ProjectileCommand {
     private static LiteralArgumentBuilder<CommandSourceStack> builder() {
         return Commands.literal("projectile")
                 .then(Commands.literal("arrow")
-                        .executes(ctx -> {
-                            if(!(ctx.getSource().getSender() instanceof Player player)) return 0;
-                            ItemStack arrowItem = ItemStack.of(Material.SPECTRAL_ARROW);
-                            DamagingAttributes attr = new DamagingAttributes(0.1, false, 0.5);
-                            ItemMeta meta = SentinelDataWrapper.getInstance().savePDC(arrowItem.getItemMeta(), attr);
-                            arrowItem.setItemMeta(meta);
-                            player.getInventory().addItem(arrowItem);
-                            SentinelDataWrapper.getInstance().loadPDC(arrowItem.getItemMeta(), DamagingAttributes.class).ifPresent(attributes ->  {
-                                System.out.println(attributes.getDamage() + " " + attributes.hasGravity());
-                            });
-                            return Command.SINGLE_SUCCESS;
-                        })
+                        .then(Commands.argument("damage", DoubleArgumentType.doubleArg(0.1))
+                        .then(Commands.argument("radius", DoubleArgumentType.doubleArg(0.1))
+                        .then(Commands.argument("gravity", BoolArgumentType.bool())
+                            .executes(ctx -> {
+                                if(!(ctx.getSource().getSender() instanceof Player player)) return 0;
+                                ItemStack arrowItem = ItemStack.of(Material.SPECTRAL_ARROW);
+                                double damage = DoubleArgumentType.getDouble(ctx, "damage");
+                                boolean gravity = BoolArgumentType.getBool(ctx, "gravity");
+                                double radius = DoubleArgumentType.getDouble(ctx, "radius");
+                                DamagingAttributes attr = new DamagingAttributes(damage, gravity, radius);
+                                SentinelDataWrapper.getInstance().savePDC(arrowItem, attr);
+                                player.getInventory().addItem(arrowItem);
+                                return Command.SINGLE_SUCCESS;
+                            })
+                ))))
+                .then(Commands.literal("flare")
+                        .then(Commands.argument("count", IntegerArgumentType.integer(1))
+                        .then(Commands.argument("health", DoubleArgumentType.doubleArg(0.1))
+                        .then(Commands.argument("healing", DoubleArgumentType.doubleArg(0.1))
+                        .then(Commands.argument("gravity", BoolArgumentType.bool())
+                                .executes(ctx -> {
+                                    if(!(ctx.getSource().getSender() instanceof Player player)) return 0;
+                                    ItemStack flareItem = ItemStack.of(Material.RED_CANDLE);
+                                    int count = IntegerArgumentType.getInteger(ctx, "count");
+                                    double health = DoubleArgumentType.getDouble(ctx, "health");
+                                    double healing = DoubleArgumentType.getDouble(ctx, "healing");
+                                    boolean gravity = BoolArgumentType.getBool(ctx, "gravity");
+                                    FlareAttributes attr = new FlareAttributes(player, gravity, healing, health, count);
+                                    SentinelDataWrapper.getInstance().savePDC(flareItem, attr);
+                                    player.getInventory().addItem(flareItem);
+                                    return Command.SINGLE_SUCCESS;
+                                })
+                        ))))
                 );
+
     }
 
     public static LiteralCommandNode<CommandSourceStack> command() {
