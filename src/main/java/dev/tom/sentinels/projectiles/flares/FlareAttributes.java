3013df5b707d3cf36b89061daa02b162c824b5e1
tom-devv <73348376+tom-devv@users.noreply.github.com>
2025-06-11 23:53:03 +0100

    add radius attribute

--- src/main/java/dev/tom/sentinels/projectiles/flares/FlareAttributes.java
+++ src/main/java/dev/tom/sentinels/projectiles/flares/FlareAttributes.java
@@ -1,5 +1,6 @@
-package dev.tom.sentinels.data;
+package dev.tom.sentinels.projectiles.flares;
 
+import dev.tom.sentinels.projectiles.Gravity;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -11,13 +12,23 @@ public class FlareAttributes implements Gravity, java.io.Serializable {
     private double mobHealth;
     private double healingPerTick;
     private boolean gravity;
+    private int searchRadius;
 
-    public FlareAttributes(Player player, boolean gravity, double healingPerTick, double mobHealth, int mobCount) {
+    public FlareAttributes(Player player, boolean gravity, double healingPerTick, double mobHealth, int mobCount, int searchRadius) {
         this.firedBy = player.getUniqueId();
         this.gravity = gravity;
         this.healingPerTick = healingPerTick;
         this.mobHealth = mobHealth;
         this.mobCount = mobCount;
+        this.searchRadius = searchRadius;
+    }
+
+    public void setSearchRadius(int searchRadius) {
+        this.searchRadius = searchRadius;
+    }
+
+    public int getSearchRadius() {
+        return searchRadius;
     }
 
     public UUID getFiredBy() {
