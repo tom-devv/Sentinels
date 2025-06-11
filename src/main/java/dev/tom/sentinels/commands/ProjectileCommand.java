e16b26cf78c711f10ba9b4f5f8b90aa077a22794
tom-devv <73348376+tom-devv@users.noreply.github.com>
2025-06-11 23:53:17 +0100

    add radius attribute

--- src/main/java/dev/tom/sentinels/commands/ProjectileCommand.java
+++ src/main/java/dev/tom/sentinels/commands/ProjectileCommand.java
@@ -8,8 +8,8 @@ import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-import dev.tom.sentinels.data.DamagingAttributes;
-import dev.tom.sentinels.data.FlareAttributes;
+import dev.tom.sentinels.projectiles.shells.ShellAttributes;
+import dev.tom.sentinels.projectiles.flares.FlareAttributes;
 import dev.tom.sentinels.data.SentinelDataWrapper;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
@@ -34,7 +34,7 @@ public class ProjectileCommand {
                                 double damage = DoubleArgumentType.getDouble(ctx, "damage");
                                 boolean gravity = BoolArgumentType.getBool(ctx, "gravity");
                                 double radius = DoubleArgumentType.getDouble(ctx, "radius");
-                                DamagingAttributes attr = new DamagingAttributes(damage, gravity, radius);
+                                ShellAttributes attr = new ShellAttributes(damage, gravity, radius);
                                 SentinelDataWrapper.getInstance().savePDC(arrowItem, attr);
                                 player.getInventory().addItem(arrowItem);
                                 return Command.SINGLE_SUCCESS;
@@ -45,19 +45,21 @@ public class ProjectileCommand {
                         .then(Commands.argument("health", DoubleArgumentType.doubleArg(0.1))
                         .then(Commands.argument("healing", DoubleArgumentType.doubleArg(0.1))
                         .then(Commands.argument("gravity", BoolArgumentType.bool())
-                                .executes(ctx -> {
-                                    if(!(ctx.getSource().getSender() instanceof Player player)) return 0;
-                                    ItemStack flareItem = ItemStack.of(Material.RED_CANDLE);
-                                    int count = IntegerArgumentType.getInteger(ctx, "count");
-                                    double health = DoubleArgumentType.getDouble(ctx, "health");
-                                    double healing = DoubleArgumentType.getDouble(ctx, "healing");
-                                    boolean gravity = BoolArgumentType.getBool(ctx, "gravity");
-                                    FlareAttributes attr = new FlareAttributes(player, gravity, healing, health, count);
-                                    SentinelDataWrapper.getInstance().savePDC(flareItem, attr);
-                                    player.getInventory().addItem(flareItem);
-                                    return Command.SINGLE_SUCCESS;
-                                })
-                        ))))
+                        .then(Commands.argument("radius", IntegerArgumentType.integer(1))
+                            .executes(ctx -> {
+                                if(!(ctx.getSource().getSender() instanceof Player player)) return 0;
+                                ItemStack flareItem = ItemStack.of(Material.RED_CANDLE);
+                                int count = IntegerArgumentType.getInteger(ctx, "count");
+                                double health = DoubleArgumentType.getDouble(ctx, "health");
+                                double healing = DoubleArgumentType.getDouble(ctx, "healing");
+                                boolean gravity = BoolArgumentType.getBool(ctx, "gravity");
+                                int radius = IntegerArgumentType.getInteger(ctx, "radius");
+                                FlareAttributes attr = new FlareAttributes(player, gravity, healing, health, count, radius);
+                                SentinelDataWrapper.getInstance().savePDC(flareItem, attr);
+                                player.getInventory().addItem(flareItem);
+                                return Command.SINGLE_SUCCESS;
+                            })
+                        )))))
                 );
 
     }
